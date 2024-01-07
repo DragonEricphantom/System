43257955279954f5bd806b8a12f5da98fddb7a79
Erie <yangg5921@gmail.com>
2024-01-07 21:11:19 +0800

    add vite Vue for FontEnd

---
+++ viteFontEnd/src/components/getStart.vue
@@ -0,0 +1,24 @@
+<script>
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const greeting = ref("第六天而已，還好嗎？");
+
+        return {
+            greeting,
+        };
+    },
+};
+</script>
+
+<template>
+    <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style>
+.greeting {
+    color: red;
+    font-weight: bold;
+}
+</style>
